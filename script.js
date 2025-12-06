60e259502bde885b3e67bb83c889c03a7a7b5219
anAvgCoder <anavgcoder@gmail.com>
2025-12-06 13:27:01 -0700

    broke 500$

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const GOAL = 500;
-  const CURRENT_AMOUNT = 479;
+  const CURRENT_AMOUNT = 503;
 
   const amountSpan = document.getElementById("goal-amount");
   const barFill = document.getElementById("goal-bar-fill");
