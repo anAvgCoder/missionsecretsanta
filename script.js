b299fb7da39fc738b20a91b55268ae73382bbc4b
anAvgCoder <anavgcoder@gmail.com>
2025-12-20 13:07:47 -0700

    update

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const GOAL = 1000;
-  const CURRENT_AMOUNT = 735;
+  const CURRENT_AMOUNT = 802;
 
   const amountSpan = document.getElementById("goal-amount");
   const barFill = document.getElementById("goal-bar-fill");
